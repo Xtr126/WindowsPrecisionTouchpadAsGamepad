013579bf053057322c10191ee5d92d51cfee035d
Xtr126 <k.gunetilleke@gmail.com>
2025-10-10 16:45:41 +0530

    Get touchpad input in background

--- TouchpadHelper.cs
+++ TouchpadHelper.cs
@@ -297,7 +297,7 @@ namespace RawInput.Touchpad
 			{
 				usUsagePage = 0x000D,
 				usUsage = 0x0005,
-				dwFlags = 0, // WM_INPUT messages come only when the window is in the foreground.
+				dwFlags = RIDEV_INPUTSINK, // WM_INPUT messages come even when the window is not in the foreground.
 				hwndTarget = windowHandle
 			};
 
