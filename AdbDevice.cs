6cbc36ef95b1ec9fc435aede11ef2e48ed2e51ca
Xtr126 <k.gunetilleke@gmail.com>
2025-10-17 13:06:43 +0530

    Update AdbDevice.cs

--- AdbDevice.cs
+++ AdbDevice.cs
@@ -3,17 +3,19 @@ namespace Gamepad.Touchpad
 
     internal class AdbDevice
     {
+        public string Device { get; set; }
         public string Serial { get; set; }
+        public bool Streaming { get; set; }
+        public bool Installed { get; set; }
         public string State { get; set; }
         public string Model { get; set; }
         public string Product { get; set; }
-        public string Device { get; set; }
         public string TransportId { get; set; }
-        public bool Selected { get; set; }
         
         public AdbDevice()
         {
-            Selected = false;
+            Streaming = false;
+            Installed = false;
         }
     }
 }
